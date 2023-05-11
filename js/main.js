3a92de51eac94e3fd0655f7d2b2a0eff0345ee7a
AllenOtto <kephamoturi@gmail.com>
2023-05-12 00:29:09 +0300

    Found images of what i think is a pokemon

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 document.querySelector('button').addEventListener('click', fetchData)
 
 async function fetchData() {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/`)
-    const jsonData = await res.json()
+    const url1 = `https://pokeapi.co/api/v2/pokemon/`
+    const response = await fetch(url1)
+    const jsonData = await response.json()
     console.log(jsonData)
+    const anotherUrl = displayFunc(jsonData)
+    fetchPokemonImage(anotherUrl)
+}
+
+const displayFunc = function display(data) {
+    const numberOfPokemon = data.results.length
+    let randomPokemon = Math.floor(Math.random() * numberOfPokemon)
+    document.querySelector('h3').innerText = data.results[randomPokemon].name
+    return data.results[randomPokemon].url
+}
+
+async function fetchPokemonImage(url) {
+    const response = await fetch(url)
+    const jsonData = await response.json()
+    console.log(jsonData)
+    console.log(jsonData.sprites.back_default)
+    document.querySelector('img').src = jsonData.sprites.back_default
 }
